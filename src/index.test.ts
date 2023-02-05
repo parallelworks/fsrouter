71b8424658638b8851222b8b68d084101fe61afc
Michael McQuade <michael@giraffesyo.io>
2023-02-04 18:24:52 -0600

    Generalize test to use node path

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,12 @@
 import { getFiles, initFsRouting } from '.'
+import path from 'path'
 
+const testroutesPath = path.join(__dirname, 'testroutes')
 describe('routes', () => {
   it('should find all files in testroutes', async () => {
-    const files = await getFiles('./src/testroutes')
-
-    expect(files).toEqual(['./src/testroutes/index.ts'])
+    const files = await getFiles(testroutesPath)
+    const fullPath = relativePath => path.join(testroutesPath, relativePath)
+    const expectedFiles = [fullPath('index.ts')]
+    expect(files).toEqual(expectedFiles)
   })
 })
