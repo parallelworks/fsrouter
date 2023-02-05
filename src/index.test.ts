523888ddb064bfbafe3446aebbbd4c5ba7658dd1
Michael McQuade <michael@giraffesyo.io>
2023-02-04 18:42:46 -0600

    Add test which hits mocked route, and gets expected response

--- src/index.test.ts
+++ src/index.test.ts
@@ -21,6 +21,26 @@ describe('routes', () => {
 
     expect(router.stack).toHaveLength(1)
   })
+  it('can hit the mounted route', async () => {
+    const router = await initFsRouting({
+      ensureAdmin: jest.fn(),
+      ensureAuthenticated: jest.fn(),
+      routesPath: fullPath(testroutesPath + '/index.ts'), // mount only the index.ts file
+      logMounts: false,
+    })
+
+    const res = {
+      json: jest.fn(),
+    }
+    const req = {
+      method: 'GET',
+    }
+    const next = jest.fn()
+    // its the only route, so its at index 1, middlware is at index 0
+    const route = router.stack[0].route.stack[1]
+    route.handle(req, res, next)
+    expect(res.json).toHaveBeenCalledWith({ message: 'Hello World!' })
+  })
 })
 
 //     const router = await initFsRouting({
